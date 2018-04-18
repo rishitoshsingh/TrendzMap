45d76f4009036b9b79ccaded9d574f2f0a539665
rishitoshsingh <rishitoshs@gmail.com>
2018-04-18 22:52:19 +0530

    Updated

--- process.py
+++ process.py
@@ -11,40 +11,58 @@ import json
 import MySQLdb
 # from mysql.connector import MySQLConnection, Error
 # from python_mysql_dbconfig import read_db_config
+import googlemaps
+# gmaps = googlemaps.Client(key='AIzaSyDLeeuz2J1-KVSPXOiBd7hoBIQYXA9BIE4')
+gmaps = googlemaps.Client(key='AIzaSyD3x9NEdpq8zBGQmV6pXSlD3vACBNChKhQ')
 
-def insert_tag(tag):
-    query = "INSERT INTO tags(tag) VALUES ('%s');" % tag
+def extract_country(location):
+    geocode_result = gmaps.geocode(location)
+    address_list = geocode_result[0]['address_components']
+    for item in address_list:
+        if (item['types'][0] == 'country'):
+            country = item['long_name']
+            return country
+        else:
+            pass
+    return 0
+
+
+def insert_tag(typ,tag_mention,country,tweet_id):
+    # query = "INSERT INTO tags(tag) VALUES ('%s');" % tag
+    query = "INSERT INTO trendz(typ,tag_mention,country,tweet_id) VALUES ('%s','%s','%s','%s');" % (typ,tag_mention,country,tweet_id)
     try:
         conn = MySQLdb.connect("localhost","rishi","","trendzmap",charset="utf8mb4")
         cursor = conn.cursor()
-        count_query = "SELECT tag FROM tags WHERE tag = '%s';" % tag
+        # count_query = "SELECT tag FROM tags WHERE tag = '%s';" % tag
+        count_query = "SELECT tag_mention FROM trendz WHERE tag_mention = '%s' AND typ = '%s';" % (tag_mention,typ)
         cursor.execute(count_query)
         row = cursor.rowcount
         if(row is 0):
             cursor.execute(query)
-            print("Tag Inserted")
+            print("Inserted")
         else:
-            get_count = "SELECT total_count FROM tags WHERE tag = '%s';" % (tag)
+            # get_count = "SELECT total_count FROM tags WHERE tag = '%s';" % (tag)
+            get_count = "SELECT total_count FROM trendz WHERE tag_mention = '%s' AND typ = '%s';" % (tag_mention,typ)
             cursor.execute(get_count)
             count = cursor.fetchone()[0]
             count+=1
-            update_query = "UPDATE tags SET total_count = '%s' WHERE TAG = '%s';" % (count,tag)         
+            update_query = "UPDATE trendz SET total_count = '%s' WHERE tag_mention = '%s' AND typ = '%s';" % (count,tag_mention,typ)         
             cursor.execute(update_query)
-            print("Tag Updated")
+            print("Updated")
         conn.commit()
     except MySQLdb.Error as e:
+        print('---------------------------------------------------------------------------------------------------------------------------------------------------------')
         print(e)
-        print(tag)
+        print(typ)
+        print(tag_mention)
+        print(country)
+        print('---------------------------------------------------------------------------------------------------------------------------------------------------------')
     finally:
         conn.close()
 
 def insert_tweet(tweet,username,tweet_id):
-    #query = "INSERT INTO tweets(tweet,username,pnr,prediction,tweet_id) VALUES ('%s','%s',%s,%s,%s);" % (tweet,username,str(pnr),str(int(prediction)))
-    #query = "INSERT INTO tweets(tweet,username,pnr,prediction,tweet_id) VALUES ('"+tweet+"','"+username+"',"+str(pnr)+","+str(int(prediction))+","+str(tweet_id)+");"
-    # query = "INSERT INTO tweets(tweet, username, tweet_id) VALUES ('"+tweet+"','"+username+"',"+str(tweet_id)+");"
     query = "INSERT INTO tweets(tweet,username,tweet_id) VALUES ('%s','%s','%s');" % (str(tweet),username,str(tweet_id))
     try:
-        # conn = MySQLdb.connect("localhost","rishi","","twitter")
         conn = MySQLdb.connect("localhost","rishi","","trendzmap",charset="utf8mb4")
         cursor = conn.cursor()
         cursor.execute(query)
@@ -81,19 +99,73 @@ def process_data(data):
             pass
 
 
-def process_tag(tag):
+# def process_tag(tag):
+
+#         print("Processing data ...")        
+
+#         if (not tag.isEmpty()):
+#             temp = []
+#             for lis in tag.collect():
+#                 if (lis):
+#                     for hastag in lis:
+#                         insert_tag(str(hastag['text']))        
+#         else:
+#             print("Empty RDD !!!")        
+#             pass
+
+
+def process_tag(data):
 
         print("Processing data ...")        
 
-        if (not tag.isEmpty()):
+        if (not data.isEmpty()):
             temp = []
-            for lis in tag.collect():
-                if (lis):
-                    for hastag in lis:
-                        insert_tag(str(hastag['text']))        
+            for tag_lis,user_mention_lis,tweet_location,user_location,tweet_id in data.collect():
+                country = ''
+                if (tag_lis):
+                    if (tweet_location):
+                        country = tweet_location['country']
+                    else :
+                        if (user_location == None):
+                            print("location = None")
+                            pass
+                        try:
+                            country = extract_country(user_location)
+                            print("Found " + country + " for "+ user_location)
+                                                    
+                        except Exception as e:
+                            print("+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++")
+                            print(e)
+                            print(len(user_location))
+                            pass
+                        
+                    for hastag in tag_lis:
+                        insert_tag('#',str(hastag['text']),country,tweet_id)
+                country = ''
+                if (user_mention_lis):
+                    if (tweet_location):
+                        country = tweet_location['country']
+                    else :
+                        if (user_location == None):
+                            print("location = None")
+                            pass
+                        try:
+                            country = extract_country(user_location)
+                            print("Found " + country + " for "+ user_location)
+                                                       
+                        except Exception as e:
+                            print("+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++")
+                            print(e)
+                            print(len(user_location))
+                            pass
+                    for mention in user_mention_lis:
+                        insert_tag('@',str(mention['screen_name']),country,tweet_id)
         else:
             print("Empty RDD !!!")        
             pass
+
+
+
 
 conf = SparkConf().setMaster("local[2]").setAppName("Streamer")
 sc = SparkContext(conf=conf)
@@ -111,8 +183,8 @@ twitter=tweets.map(lambda tweet: tweet['user']['screen_name'])
 tweet_text = tweets.map(lambda tweet: tweet['text'])
 
 txt = tweets.map(lambda x: (x['text'], x['user']['screen_name'], x['id']))
-tags = tweets.map(lambda x: (x['entities']['hashtags']))
-txt.foreachRDD(process_data)
+tags = tweets.map(lambda x: (x['entities']['hashtags'],x['entities']['user_mentions'],x['place'],x['user']['location'],x['id']))
+# txt.foreachRDD(process_data)
 tags.foreachRDD(process_tag)
 
 ssc.start() 
